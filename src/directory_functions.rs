71bd7b970987a991b7a9e913b2d0468875c1455f
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-08-21 14:38:32 -0500

    Made the make_project_directory() function also make the words_list directory in the data directory, renamed function accordingly

--- src/directory_functions.rs
+++ src/directory_functions.rs
@@ -3,13 +3,16 @@ use std::fs;
 use std::path::Path;
 use directories::ProjectDirs;
 
-pub fn make_project_directory(project_directory: ProjectDirs) {
+pub fn make_project_directories(project_directory: ProjectDirs) {
     // Makes the directories the project needs for stuff
     // At the moment this is only a data directory, however, later on I plan on making a config directory.
     let project_directory = ProjectDirs::from("", "Deranged Lunatic Apps", "rusty-fish").unwrap();
-    let _ = match fs::create_dir_all(project_directory.data_dir()) {
-        Ok(_) => println!("Sucessfully made directory {:?}", project_directory.data_dir()),
-        Err(_) => println!("Directory {:?} failed to be made!", project_directory.data_dir())
+    let words_list_directory = project_directory.data_dir().join(Path::new("words_list"));
+
+    // This will also make the parent directories so no need to worry about those
+    let _ = match fs::create_dir_all(&words_list_directory) {
+        Ok(_) => println!("Sucessfully made directory {:?}", words_list_directory),
+        Err(_) => println!("Directory {:?} failed to be made!", words_list_directory)
     };
 }
 
