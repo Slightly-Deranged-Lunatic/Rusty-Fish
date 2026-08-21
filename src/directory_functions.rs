536be5480ce8408d9a274ccfa8918845b41333c7
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-08-21 16:23:25 -0500

    A little bit of function cleanup

--- src/directory_functions.rs
+++ src/directory_functions.rs
@@ -4,13 +4,12 @@ use std::path::Path;
 use directories::ProjectDirs;
 use reqwest;
 
-pub fn make_project_directories(project_directory: ProjectDirs) {
+pub fn make_project_directories(project_directory: & ProjectDirs) {
     // Makes the directories the project needs for stuff
     // At the moment this is only a data directory, however, later on I plan on making a config directory.
     // The above is why this is a function and not just in download_words_list()
-    let project_directory = ProjectDirs::from("", "Deranged Lunatic Apps", "rusty-fish").unwrap();
-    let words_list_directory = project_directory.data_dir().join(Path::new("words_list"));
 
+    let words_list_directory = project_directory.data_dir().join(Path::new("words_list"));
     // This will also make the parent directories so no need to worry about those
     let _ = match fs::create_dir_all(&words_list_directory) {
         Ok(_) => println!("Sucessfully made directory {:?}", words_list_directory),
@@ -18,16 +17,12 @@ pub fn make_project_directories(project_directory: ProjectDirs) {
     };
 }
 
-pub fn download_words_list(project_directory: ProjectDirs) {
+pub fn download_words_list(project_directory: & ProjectDirs) {
     // Create project_directory.datadir()/words_list
     let words_list_directory = project_directory.data_dir().join(Path::new("words_list"));
-    let _ = match fs::create_dir(& words_list_directory) {
-        Ok(_) => println!("Successfully made directory {:?}", words_list_directory),
-        Err(_) => println!("Directory {:?} failed to be made!", words_list_directory)
-    };
-
     let words_lists_files = vec!["easy_words.json", "normal_words.json","hard_words.json"];
     let base_url = "https://raw.githubusercontent.com/Slightly-Deranged-Lunatic/Rusty-Fish/refs/heads/main/words_lists/";
+
     // Actually download the file from the repository
     for list in words_lists_files {
         let url = format!("{}{}", base_url, list);
