0cb1967f13c6e450b37fe943951569e0b5e63bc5
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-08-21 17:42:58 -0500

    Function to determine whether or not the words list needs to be downloaded

--- src/directory_functions.rs
+++ src/directory_functions.rs
@@ -1,6 +1,6 @@
 /// Module has all the code related to directories
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use directories::ProjectDirs;
 use reqwest;
 
@@ -33,5 +33,34 @@ pub fn download_words_list(project_directory: & ProjectDirs) {
             Ok(_) => println!("Successfully made file {:?}", words_list_file),
             Err(_) => println!("Failed to make file {:?}", words_list_file)
         };
+    }
+}
+
+pub fn should_download_words_list(project_directory: & ProjectDirs) -> bool {
+    // Determines if the word list files are there and whether or not they should be downloaded.
+    // Returns true if we need to download the words lists again
+    // Returns false if not
+
+    let words_list_directory = project_directory.data_dir().join(Path::new("words_list"));
+    let files = fs::read_dir(words_list_directory).unwrap();
+    let mut local_files: Vec<PathBuf> = Vec::new();
+    for path in files {
+        let file = PathBuf::from(path.unwrap().path()
+        .components().last()
+        .unwrap().as_os_str());
+        local_files.push(file);
+    };
+    let mut words_lists_files = vec![
+        PathBuf::from("easy_words.json"),
+        PathBuf::from("normal_words.json"),
+        PathBuf::from("hard_words.json")];
+
+    words_lists_files.sort();
+    local_files.sort();
+
+    if local_files == words_lists_files {
+        return false;
+    } else {
+        return true;
     }
 }
