5b4a245d447d6cddbd804dbb7859dc20ae6e8b58
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-08-25 16:16:57 -0500

    Add a temporary quit to the fishing menu

--- src/update.rs
+++ src/update.rs
@@ -17,6 +17,7 @@ pub fn update(app: &mut App, key_event: KeyEvent) {
             _ => {}
         }
     } else if  app.window == "Fishing" {
+        if key_event.code == KeyCode::Char('q') {app.quit()} // TEMPORARY PLEASE DONT FORGET TO DELETE THIS
         match key_event.code.as_char() {
             Some(character) => app.insert_text(character),
             None => log::info!("Failed to convert {} to a char", key_event.code)
