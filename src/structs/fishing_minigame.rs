944293bb0c064105423cfebcc8dfb2ce63a75fdf
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-09-08 13:54:34 -0500

    Fishing minigame struct to organize the code used for fishing minigames better

---
+++ src/structs/fishing_minigame.rs
@@ -0,0 +1,37 @@
+use std::time::Instant;
+use crate::{structs::fish::Fish};
+pub struct FishingMinigame {
+    pub typed_text: Vec<char>,
+    pub start_time: Instant,
+    pub words: Vec<char>,
+    pub elasped_time: f32,
+    pub accuracy: f32,
+    pub wpm: f32,
+    pub catch: Fish
+}
+
+impl FishingMinigame {
+    pub fn new(words: Vec<char>, catch: Fish) -> Self {
+        FishingMinigame {
+            typed_text: Vec::new(),
+            start_time: Instant::now(),
+            words: words,
+            elasped_time: 0.0,
+            accuracy: 0.0,
+            wpm: 0.0,
+            catch: catch,
+        }
+    }
+
+    pub fn insert_text(&mut self, character: char) {
+        self.typed_text.push(character);
+    }
+
+    pub fn pop_typed_text(&mut self) {
+        self.typed_text.pop();
+    }
+
+    pub fn clear_typed_text(&mut self) {
+        self.typed_text.clear();
+    }
+}
