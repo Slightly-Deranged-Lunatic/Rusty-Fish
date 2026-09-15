0799140f1d317fcb959f4bcdeb26f1c931ee1293
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-09-15 18:48:39 -0500

    Some new attributes for the auto scrolling

--- src/structs/fishing_minigame.rs
+++ src/structs/fishing_minigame.rs
@@ -1,4 +1,5 @@
 use std::time::Instant;
+
 use crate::{structs::fish::Fish};
 pub struct FishingMinigame {
     pub typed_text: Vec<char>,
@@ -7,6 +8,8 @@ pub struct FishingMinigame {
     pub elasped_time: f32,
     pub accuracy: f32,
     pub wpm: f32,
+    pub current_line: usize,
+    pub position_in_line: i32,
     pub catch: Fish
 }
 
@@ -19,16 +22,21 @@ impl FishingMinigame {
             elasped_time: 0.0,
             accuracy: 0.0,
             wpm: 0.0,
+            current_line: 0,
+            position_in_line: 0,
             catch: catch,
         }
     }
 
     pub fn insert_text(&mut self, character: char) {
         self.typed_text.push(character);
+        self.position_in_line += 1;
+        
     }
 
     pub fn pop_typed_text(&mut self) {
         self.typed_text.pop();
+        self.position_in_line -= 1;
     }
 
     pub fn clear_typed_text(&mut self) {
