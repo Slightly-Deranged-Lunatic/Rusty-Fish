1fdf7fe8904b305bd24f0c8aa1d7e588ada0b098
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-09-16 12:56:39 -0500

    Add functions to get styles for certain elements

--- src/ui.rs
+++ src/ui.rs
@@ -1,3 +1,4 @@
 pub mod fishing_ui;
 pub mod general_ui;
 pub mod menu_ui;
+pub mod ui_styles;
---
+++ src/ui/ui_styles.rs
@@ -0,0 +1,34 @@
+use ratatui::style::{Style, Color, Modifier};
+
+pub fn list_style() -> Style {
+    const LIST_STYLE: Style  = Style::new()
+    .fg(Color::Magenta);
+    
+    return LIST_STYLE;
+}
+
+pub fn correct_character_style() -> Style {
+    const CORRECT_CHARACTER_STYLE: Style = Style::new()
+    .fg(Color::Magenta)
+    .underline_color(Color::Magenta)
+    .add_modifier(Modifier::UNDERLINED);
+
+    return CORRECT_CHARACTER_STYLE;
+}
+
+pub fn incorrect_character_style() -> Style {
+    const INCORRECT_CHARACTER_STYLE: Style = Style::new()
+    .fg(Color::Red)
+    .underline_color(Color::Red)
+    .add_modifier(Modifier::UNDERLINED);
+
+    return INCORRECT_CHARACTER_STYLE;
+}
+
+pub fn untyped_character_style() -> Style {
+    const UNTYPED_CHARACTER_STYLE: Style = Style::new()
+    .fg(Color::Gray);
+
+    return UNTYPED_CHARACTER_STYLE;
+}
+
